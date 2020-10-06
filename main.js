eeff97ad167add6fc1dcb0dc80dce6676025632a
François Brucker <francois.brucker@centrale-marseille.fr>
2020-10-06 14:30:19 +0200

    add numerologie

---
+++ main.js
@@ -0,0 +1,16 @@
+function change_value() {
+    prenom = document.querySelector("#prenom").value
+    if (prenom) {
+        document.querySelector("#get-value").textContent = nombre(prenom)
+    } else {
+        console.log("prout")
+        document.querySelector("#get-value").textContent = "?"
+    }
+
+}
+change_value()
+
+document.querySelector("#button-form").addEventListener("click", (event) => {
+    change_value()
+    event.preventDefault()
+})
